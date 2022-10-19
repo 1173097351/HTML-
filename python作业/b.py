c7326d8c0bdd9f971e7af9f9fb30018ff69ad047
root <1173097351@qq.com>
2022-10-19 11:47:06 +0800

    第四次作业

--- python作业/b.py
+++
@@ -1,8 +0,0 @@
-counter = 0
-s = 9
-while True:
-    s *= 2
-    counter += 1
-    if s > 884800000:
-        break
-print("需要对折:", counter)
--- python作业/main.py
+++
@@ -1,6 +0,0 @@
-a = int(input("请输入一个整数输入："))
-c = 0
-while a > 1:
-    a /= 10
-    c += 1
-print(c)
